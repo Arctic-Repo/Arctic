6db7be6ab391cf3f5c7e2040da1e3ec84cfc1ccf
Thyke Adams <45541936+ThykeAdams@users.noreply.github.com>
2021-03-03 03:15:25 -0500

    :pig:

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 $( "#snow" ).hide();
 $( "#snow" ).fadeIn(2000);
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 400) {
+    if ($(this).scrollTop() > 40) {
         $( ".header #background" ).fadeIn();
         $( "#snow" ).fadeOut();
     } else {
